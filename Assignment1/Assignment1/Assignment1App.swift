06327801f17cd69f8b47d0fab7fcf2c9e3347897
raikidnt <118874686+raikidnt@users.noreply.github.com>
2024-07-21 16:42:42 +0700

    Initial commit test to make sure github repo is connected

---
+++ Assignment1/Assignment1/Assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1App.swift
+//  Assignment1
+//
+//  Created by Do Thanh on 21.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment1/Assignment1/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Assignment1
+//
+//  Created by Do Thanh on 21.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("12345")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
